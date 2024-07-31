0af1d07b9180f9e646e57e11d2cde2d136bfe3b0
dhuruv0003 <116896454+dhuruv0003@users.noreply.github.com>
2024-07-31 17:47:27 +0530

    new commit

--- Backend/Controller/userController.js
+++ Backend/Controller/userController.js
@@ -1,5 +1,17 @@
+import ErrorHandler from "../MiddleWares/ErrorHandler";
 import { UserMod } from "../Models/userSchema";
+
 
 const register=async(req,res,next)=>{
     const {FirstName,LastName,Email,Phone,NIC, dob,gender,password,role}=req.body;
+    if(!FirstName || !LastName || !Email||!Phone,NIC || !dob || !gender || !password || !role){
+        return next(new ErrorHandler(400,"please fill the form properly"))
+    }
+
+    const userExist= await UserMod.findOne({email});
+    if(userExist){
+        return next(new ErrorHandler(400,"user already exists"));
+    }
+    
+
 }
