1b9a48fc9b7194bedf89eb5161601498675f970b
dhuruv0003 <116896454+dhuruv0003@users.noreply.github.com>
2024-08-05 18:05:51 +0530

    new commit

---
+++ Controller/appointmentController.js
@@ -0,0 +1,55 @@
+import { catchAsyncErrors } from "../MiddleWares/catchAsyncErrors";
+import ErrorHandler from "../MiddleWares/ErrorHandler";
+import { AppointmentModel } from "../Models/appointMentSchema";
+import { UserMod } from "../Models/userSchema";
+
+
+export const postAppointment=catchAsyncErrors(async(req,res,next)=>{
+    const {FirstName,LastName,Email,Phone,NIC,DOB,Gender,Appointment_date,Department,Doctor_firstName,Doctor_lastName,HasVisited,Address}=req.body;
+
+    if(!FirstName || !LastName || !Email || !Phone || !NIC || !DOB || !Gender || !Appointment_date || !Department || !Doctor_firstName || !Doctor_lastName  || !Address){
+        return next(new ErrorHandler(400, "please Fill details properly"))
+    }
+
+    // If there are more then one doctor in same department with same name, then raise error.
+    // Note all the doctor's are register in userMod, so verify from there only
+
+    const isDoctorConflict=await UserMod.find({
+        FirstName:Doctor_firstName,
+        LastName:Doctor_lastName,
+        Role:"Doctor",
+        DoctorDepart:Department
+    })
+    // above code return array with doctor having same name 
+
+    if(isDoctorConflict.length===0){
+        return next(new ErrorHandler(404,"Doctor with the given name not found"))
+    }
+
+    if(isDoctorConflict.length>1){
+        return next(new ErrorHandler(400,
+            "More than one doctor with given name exists in same department, please contact through email"
+        ));
+    };
+
+    // if any doctor found
+    const DoctorId=isDoctorConflict[0]._id
+    // jaise hi patient login k rega, uska payload req.user me store ho jayega 
+    const PatientId=req.user._id
+
+    const dbEntry=await AppointmentModel.create({
+        FirstName,LastName,Email,Phone,NIC,DOB,Gender,Appointment_date,Department,
+        Doctor:{
+            firstName:Doctor_firstName,
+            lastName:Doctor_lastName
+        },PatientId,DoctorId
+        ,HasVisited,Address
+    })
+
+    return res.status(200).json({
+        success:true,
+        message:"Doctor Registered Successfully",
+        dbEntry
+    })
+
+})
