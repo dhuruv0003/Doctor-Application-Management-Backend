fc71b4de47d2fb69b68963c08873b54558eaac39
dhuruv0003 <116896454+dhuruv0003@users.noreply.github.com>
2024-11-19 22:57:13 +0530

    new commit

--- server.js
+++ server.js
@@ -25,6 +25,8 @@ app.use(cors(
   }
 ))
 
+
+
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "http://localhost:5173");
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
